34d051ce7870640a6f774eec4de293314be06dca
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-25 13:03:26 -0600

    fix ts complie error

--- components/menu/category.tsx
+++ components/menu/category.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Text } from '@nextui-org/react';
+import { CSSProperties } from '@nextui-org/react/types/theme';
 export type CategoryProps = {
 	id: string;
 	name: string;
 };
 
-const Category: React.FC<{ category: CategoryProps }> = ({
-	category,
-	activeCategory,
-}) => {
-	const style = {
+const Category: React.FC<{
+	category: CategoryProps;
+	activeCategory: object;
+}> = ({ category, activeCategory }) => {
+	const style: CSSProperties = {
 		whiteSpace: 'nowrap',
 		padding: '3px 10px',
 		marginRight: '5px',
 		borderRadius: '25px',
 		cursor: 'pointer',
+		userSelect: 'none',
 	};
 	const selectCategory = (e) => {
 		const categories = document.getElementsByClassName(
 			'categoryNav__category'
 		);
-		for (const category of categories) {
+		for (const category of categories as any) {
 			category.style.backgroundColor = 'transparent';
 		}
 		e.target.style.backgroundColor = '#0072f5';
