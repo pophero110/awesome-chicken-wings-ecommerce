7d8fee6fe920f05ee151e3371eedf9d967b3cd14
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-25 12:21:52 -0600

    add selected category css

--- components/menu/category.tsx
+++ components/menu/category.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
-
+import { Text } from '@nextui-org/react';
+import style from './styles.module.css';
 export type CategoryProps = {
 	id: string;
 	name: string;
 };
 
-const Category: React.FC<{ category: CategoryProps }> = ({ category }) => {
+const Category: React.FC<{ category: CategoryProps }> = ({
+	category,
+	activeCategory,
+}) => {
+	const style = {
+		whiteSpace: 'nowrap',
+		padding: '0 10px',
+		marginRight: '5px',
+		borderRadius: '30px',
+	};
+	const selectCategory = (e) => {
+		const categories = document.getElementsByClassName(
+			'categoryNav__category'
+		);
+		for (const category of categories) {
+			category.style.backgroundColor = 'transparent';
+		}
+		e.target.style.backgroundColor = '#0072f5';
+	};
 	return (
-		<div className='categoryNav__category'>
-			<p>{category.name}</p>
-		</div>
+		<Text
+			onClick={selectCategory}
+			className='categoryNav__category'
+			style={{ ...style, ...activeCategory }}>
+			{category.name}
+		</Text>
 	);
 };
 
--- components/menu/categoryNav.tsx
+++ components/menu/categoryNav.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Category, { CategoryProps } from './category';
-
+import Slider from '../shared/slider/slider';
 type CategroyNavProps = {
 	categories: CategoryProps[];
 };
 const CategroyNav: React.FC<CategroyNavProps> = ({ categories }) => {
 	return (
-		<div className='categoryNav'>
-			{categories.map((category) => {
-				return (
-					<Category category={category} key={category.id}></Category>
-				);
-			})}
+		<div
+			className='categoryNav'
+			style={{ paddingBottom: '20px', paddingTop: '20px' }}>
+			<Slider>
+				{categories.map((category, index) => {
+					return (
+						<Category
+							activeCategory={
+								index === 0
+									? { backgroundColor: '#0072f5' }
+									: {}
+							}
+							category={category}
+							key={category.id}></Category>
+					);
+				})}
+			</Slider>
 		</div>
 	);
 };
