9e0b59adfb3699bba22251cff3c82da3a40ff1d7
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-25 22:33:05 -0600

    fix: mobileNavbar state reset to intial state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 import { createTheme } from '@nextui-org/react';
+import MoblieNavbar from '../components/mobileNavbar/mobileNavbar';
 const App = ({ Component, pageProps }: AppProps) => {
 	const darkTheme = createTheme({
 		type: 'dark',
@@ -8,6 +9,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<NextUIProvider theme={darkTheme}>
 			<Component {...pageProps} />
+			<MoblieNavbar></MoblieNavbar>
 		</NextUIProvider>
 	);
 };
