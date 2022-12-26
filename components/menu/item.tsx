78519428d247c324ab5c9949f0a49eebd2def3e0
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-26 15:10:25 -0600

    add minus, plus button; remove footer; display  name and price on item image

--- components/menu/item.tsx
+++ components/menu/item.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './item.module.css';
-import { Card, Text, Row } from '@nextui-org/react';
+import { Card, Text } from '@nextui-org/react';
+import Icon from '../svg/icon';
 export type ItemProps = {
 	id: string;
 	name: string;
@@ -8,23 +9,51 @@ export type ItemProps = {
 };
 
 const Item: React.FC<{ item: ItemProps }> = ({ item }) => {
+	const [displayMinus, setDisplayMinus] = useState(false);
+	const handlePlusClick = () => {
+		setDisplayMinus(true);
+	};
+	const handleMinusClick = () => {
+		setDisplayMinus(false);
+	};
 	return (
-		<Card isPressable className={style.menu__item}>
+		<Card className={style.menu__item}>
 			<Card.Body css={{ p: 0 }}>
 				<Card.Image
 					src={'https://nextui.org/images/fruit-1.jpeg'}
 					objectFit='cover'
 					width='100%'
-					height={140}
+					height={150}
 					alt={item.name}
 				/>
+				<div className={style.menu__item__metadata}>
+					<Text b style={{ backgroundColor: 'transparent' }}>
+						{item.name}
+					</Text>
+					<Text color='white'>{`$${item.price}`}</Text>
+				</div>
+				<div className={style.menu__item__action}>
+					{displayMinus ? (
+						<>
+							<Icon
+								type='minus'
+								onClick={handleMinusClick}></Icon>
+							<span
+								style={{
+									paddingLeft: '5px',
+									paddingRight: '5px',
+									color: '#EAF4FF',
+								}}>
+								0
+							</span>
+						</>
+					) : (
+						''
+					)}
+
+					<Icon type='plus' onClick={handlePlusClick}></Icon>
+				</div>
 			</Card.Body>
-			<Card.Footer css={{ justifyItems: 'flex-start' }}>
-				<Row wrap='wrap' justify='space-between' align='center'>
-					<Text b>{item.name}</Text>
-					<Text color='success'>{`$${item.price}`}</Text>
-				</Row>
-			</Card.Footer>
 		</Card>
 	);
 };
