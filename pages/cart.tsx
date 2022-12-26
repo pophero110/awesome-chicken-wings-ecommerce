3c2ce7dbd1c1b875c7ba2260c9fd4faee57f52e1
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-25 21:59:03 -0600

    add a plain cart page

---
+++ pages/cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+const Cart = () => {
+	return (
+		<Layout>
+			<div>Cart</div>
+		</Layout>
+	);
+};
+
+export default Cart;
