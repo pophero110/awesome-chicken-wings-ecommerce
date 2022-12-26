d5bc646b4613f5bc4c307b25d448c82901a43148
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-26 15:08:36 -0600

    import global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
 import { createTheme } from '@nextui-org/react';
 import Layout from '../components/Layout';
+import './global.css';
 const App = ({ Component, pageProps }: AppProps) => {
 	const darkTheme = createTheme({
 		type: 'dark',
