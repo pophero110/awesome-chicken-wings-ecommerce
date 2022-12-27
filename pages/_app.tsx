a08d69846f7fabb31934c56f465dee94b9f5abfa
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-26 18:54:28 -0600

    event(app): add loading effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,29 @@ import { NextUIProvider } from '@nextui-org/react';
 import { createTheme } from '@nextui-org/react';
 import Layout from '../components/Layout';
 import './global.css';
+import Router from 'next/router';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 const App = ({ Component, pageProps }: AppProps) => {
 	const darkTheme = createTheme({
 		type: 'dark',
 	});
+
+	useEffect(() => {
+		const handleRouteStart = () => NProgress.start();
+		const handleRouteDone = () => NProgress.done();
+
+		Router.events.on('routeChangeStart', handleRouteStart);
+		Router.events.on('routeChangeComplete', handleRouteDone);
+		Router.events.on('routeChangeError', handleRouteDone);
+
+		return () => {
+			Router.events.off('routeChangeStart', handleRouteStart);
+			Router.events.off('routeChangeComplete', handleRouteDone);
+			Router.events.off('routeChangeError', handleRouteDone);
+		};
+	}, []);
 	return (
 		<NextUIProvider theme={darkTheme}>
 			<Layout>
