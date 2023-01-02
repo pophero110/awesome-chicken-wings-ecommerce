ecd22fdcddb989d09b2bced5bc5cdfc4b0a8c8b7
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-01 22:52:34 -0600

    service createOrder: unit test

---
+++ test/services/createOrder.test.ts
@@ -0,0 +1,51 @@
+import CreateOrder from '../../services/createOrder';
+import { deleteAllRecords } from '../helpers/prisma';
+import prisma from '../../lib/prisma';
+describe('process', () => {
+	let order;
+	let checkoutMode = false;
+	const itemsData = {
+		0: 1,
+		1: 1,
+	};
+	beforeAll(async () => {
+		await prisma.item.createMany({
+			data: [
+				{ id: 0, price: 15.25, name: 'Chicken Sandwich' },
+				{ id: 1, price: 9.25, name: 'Beef Sandwich' },
+			],
+			skipDuplicates: true,
+		});
+
+		const service = new CreateOrder(itemsData, checkoutMode);
+		order = await service.process();
+	});
+	afterAll(() => {
+		return deleteAllRecords();
+	});
+
+	test('it returns object with subtotal and total properties', () => {
+		expect(order.subtotal).toBe(24.5);
+		expect(order.total).toBe(24.5);
+	});
+
+	test('it does not save any data into database', async () => {
+		const orderData = await prisma.order.findMany();
+
+		expect(orderData.length).toBe(0);
+	});
+
+	describe('checkMode is true', () => {
+		beforeAll(async () => {
+			checkoutMode = true;
+			const service = new CreateOrder(itemsData, checkoutMode);
+			await service.process();
+		});
+
+		test('it saves order into database', async () => {
+			const orderData = await prisma.order.findMany();
+
+			expect(orderData.length).toBe(1);
+		});
+	});
+});
