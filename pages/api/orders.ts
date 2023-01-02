2deb47267b56af911681bf2c9c6671abb86afc8b
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-02 00:34:01 -0600

    fix: do not createPaymentIntet when checkout is false

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -11,25 +11,30 @@ const orderHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 		const service = new CreateOrder(itemsData, checkoutMode);
 		const order = await service.process();
 
-		let paymentIntent;
-		let { clientSecret, paymentIntentId } = parseCookies({ req });
-		if (!clientSecret) {
-			paymentIntent = await createPaymentIntent(order);
-			setCookie({ res }, 'paymentIntentId', paymentIntent.id);
-		} else {
-			paymentIntent = await updatePaymentIntent(paymentIntentId, order);
-		}
-
 		if (order.error) {
 			res.status(400).json({
 				error: order.error,
 			});
 		}
-		res.status(200).json({
-			subtotal: order.subtotal,
-			total: order.total,
-			clientSecret: paymentIntent.client_secret,
-		});
+		if (!checkoutMode) {
+			let paymentIntent;
+			let { clientSecret, paymentIntentId } = parseCookies({ req });
+			if (!clientSecret) {
+				paymentIntent = await createPaymentIntent(order);
+				setCookie({ res }, 'paymentIntentId', paymentIntent.id);
+			} else {
+				paymentIntent = await updatePaymentIntent(
+					paymentIntentId,
+					order
+				);
+			}
+
+			res.status(200).json({
+				subtotal: order.subtotal,
+				total: order.total,
+				clientSecret: paymentIntent.client_secret,
+			});
+		}
 	}
 };
 
