63d91fb61902571f471a2fde90e1f40bbab2456c
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-13 11:09:17 -0600

    refacotr configuration on nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcrypt';
 import prisma from '../../../lib/prisma';
-import { randomBytes, randomUUID } from 'crypto';
 export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
@@ -13,13 +12,6 @@ export const authOptions = {
 			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
 			// e.g. domain, username, password, 2FA token, etc.
 			// You can pass any HTML attribute to the <input> tag through the object.
-			credentials: {
-				email: {
-					label: 'Email',
-					type: 'text',
-				},
-				password: { label: 'Password', type: 'password' },
-			},
 			async authorize(credentials, req) {
 				const { email, password } = credentials;
 				const user = await prisma.user.findUnique({
@@ -31,11 +23,9 @@ export const authOptions = {
 				if (result) {
 					// Any object returned will be saved in `user` property of the JWT
 					return { email: user.email };
-					// return null;
 				} else {
 					// If you return null then an error will be displayed advising the user to check their details.
 					return null;
-
 					// You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
 				}
 			},
@@ -43,10 +33,10 @@ export const authOptions = {
 	],
 	pages: {
 		signIn: '/auth/signin',
-		// 	signOut: '/auth/signout',
-		// error: '/auth/signin', // Error code passed in query string as ?error=
-		// 	verifyRequest: '/auth/verify-request', // (used for check email message)
-		// 	newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
+		signOut: '/auth/signout',
+		error: '/auth/signin', // Error code passed in query string as ?error=
+		verifyRequest: '/auth/verify-request', // (used for check email message)
+		newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
 	},
 	secret: process.env.NEXTAUTH_SECRET,
 	session: {
@@ -57,7 +47,11 @@ export const authOptions = {
 		secret: process.env.NEXTAUTH_SECRET,
 		encryption: true,
 	},
-
 	useSecureCookies: process.env.NODE_ENV === 'production',
+	events: {
+		async signIn(message) {
+			console.log('signed in');
+		},
+	},
 };
 export default NextAuth(authOptions);
