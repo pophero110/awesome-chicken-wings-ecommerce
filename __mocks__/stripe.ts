1a973a1baa979b3080b009260a4fc9beac90bee2
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-12 19:57:00 -0600

    add updatePaymentIntent mock

--- __mocks__/stripe.ts
+++ __mocks__/stripe.ts
@@ -6,8 +6,13 @@ const createPaymentIntentMock = () => {
 	return { client_secret: 'clientSecret' };
 };
 
+const updatePaymentIntent = () => {
+	return { client_secret: 'clientSecret' };
+};
+
 Stripe.prototype.paymentIntents = {
 	create: createPaymentIntentMock,
+	update: updatePaymentIntent,
 };
 
 export default Stripe;
