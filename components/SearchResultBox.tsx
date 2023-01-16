e7334877a24e0b4fd042ae87fd3aa81511342b83
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-16 16:23:51 -0600

    set minWidth

--- components/SearchResultBox.tsx
+++ components/SearchResultBox.tsx
@@ -13,7 +13,8 @@ export default function SearchResultBox({ searchResult }) {
 					: 'translate3d(0%, -5%, 0px) scale(1)',
 				borderRadius: '4px',
 				backgroundColor: 'black',
-				width: '100%',
+				width: 'auto',
+				minWidth: '100%',
 				boxShadow:
 					'rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset',
 			}}>
