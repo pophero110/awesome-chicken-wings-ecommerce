f64c0c5034e7edcd552e19074d1dd1fbf3e693cb
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-16 12:53:52 -0600

    fullscreen modal on mobile

--- components/ModalContainer.tsx
+++ components/ModalContainer.tsx
@@ -4,15 +4,34 @@ import {
 	useModalContainer,
 	useSetModalContainer,
 } from '../contexts/modalContainerContext';
+import { useEffect, useState } from 'react';
 export default function ModalContainer() {
 	const { modalContainer } = useModalContainer();
 	const { setModalContainer } = useSetModalContainer();
 	const closeHandler = () => {
 		setModalContainer({ visible: false, type: 'signin' });
 	};
+	const [isFullScreen, setFullScreen] = useState(false);
+	useEffect(() => {
+		const media = window.matchMedia('(max-width: 700px)');
+		const handleFullScreen = () => {
+			if (media.matches) {
+				setFullScreen(true);
+			} else {
+				setFullScreen(false);
+			}
+		};
+		media.addEventListener('change', handleFullScreen);
+		handleFullScreen();
+		return () => {
+			media.removeEventListener('change', handleFullScreen);
+		};
+	}, []);
+
 	return (
 		<Modal
 			closeButton
+			fullScreen={isFullScreen}
 			aria-labelledby='modal-title'
 			open={modalContainer.visible}
 			onClose={closeHandler}>
